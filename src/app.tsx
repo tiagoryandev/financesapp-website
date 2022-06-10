4f6153aee38953ccf7b173e0442675b1c1de4c12
Tiago Ryan <tiagoinsany@gmail.com>
2022-06-10 17:05:59 -0300

    chore: remove light theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 
-import { LightMode, DarkMode, Themes } from "./themes";
+import { DarkMode } from "./themes";
 import { NotifyProvider } from "./contexts/NotifyContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { TrackerProvider } from "./contexts/TrackerContext";
@@ -9,18 +9,8 @@ import RouterManager from "./routes";
 import GlobalStyles from "./styles/GlobalStyles";
 
 const App: React.FC = () => {
-	const [theme, setTheme] = useState<Themes>("dark");
-
-	useEffect(() => {
-		const themeConfig = localStorage.getItem("@finances-app:theme") as Themes;
-
-		if (themeConfig) {
-			setTheme(themeConfig);
-		}
-	}, []);
-
 	return (
-		<ThemeProvider theme={theme === "light" ? LightMode : DarkMode}>		
+		<ThemeProvider theme={DarkMode}>		
 			<NotifyProvider>
 				<AuthProvider>
 					<TrackerProvider>
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,4 +1,4 @@
-export type Themes = "light" | "dark";
+export type Themes =  "dark";
 
 export const DarkMode = {
 	colors: {
@@ -16,39 +16,6 @@ export const DarkMode = {
 
 			default: "#e1e1e6",
 			strong: "#FFFFFF",
-			description: "#808080"
-		},
-		input: {
-			border: "#090909"
-		},
-		buttons: {
-			green: {
-				default: "#519259",
-				black: "#064635"
-			}
-		},
-		icon: {
-			default: "#e1e1e6",
-			strong: "#FFFFFF"
-		}
-	}
-};
-
-export const LightMode = {
-	colors: {
-		background: {
-			default: "#121212",
-			modal: "#202020",
-			red: {
-				default: "crimson"
-			},
-			green: {
-				default: "#519259"
-			}
-		},
-		text: {
-			default: "#e1e1e6",
-			strong: "#000000",
 			description: "#808080"
 		},
 		input: {
